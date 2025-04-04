b6f72e6cd6011cd2247663a18403a77ed156b9bb
Max Crawford <mrcrawfo@gmail.com>
2025-04-04 02:08:57 -0400

    fix(NavMenu): Refactored open/close functionality to hide mobile menu on page navigation

--- components/mobile/NavMenu.jsx
+++ components/mobile/NavMenu.jsx
@@ -1,15 +1,42 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
+import { usePathname } from 'next/navigation'
+
 import NavLink from '@/components/NavLink';
 
 const NavMenu = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const pathname = usePathname();
+
+    useEffect(() => {
+        closeMenu();
+    }, [setIsExpanded, pathname]);
+
+    const closeMenu = () => {
+        const menu = document.getElementById('mobile-menu');
+        setIsExpanded(false);
+        menu?.classList.add('hidden');
+    };
+    
+    const openMenu = () => {
+        const menu = document.getElementById('mobile-menu');
+        setIsExpanded(true);
+        menu?.classList.remove('hidden');
+    };
+
     return (
         <>
             <button 
             className="mobile-menu-button p-2"
             onClick={() => {
-                const menu = document.getElementById('mobile-menu');
-                menu?.classList.toggle('hidden');
+                if (isExpanded) {
+                    closeMenu();
+                } else {
+                    openMenu();
+                }
             }}
             >
                 <svg className="w-6 h-6 stroke-active-contrast" fill="none" viewBox="0 0 24 24">
@@ -17,13 +44,13 @@ const NavMenu = () => {
                 </svg>
             </button>
 
-            <ul id="mobile-menu" className="hidden absolute left-0 right-0 bg-background border-b border-foreground/10 px-0 py-4 mt-2">
-                <li className="py-2 bg-background-secondary w-screen">
+            <ul id="mobile-menu" className="hidden absolute left-0 right-0 bg-background border-b px-0  mt-2">
+                <li className="py-2 pl-8 bg-background-secondary w-screen">
                     <NavLink href="/">
                     Home
                     </NavLink>
                 </li>
-                <li className="py-2 bg-background-secondary w-screen">
+                <li className="py-2 pl-8 bg-background-secondary w-screen">
                     <NavLink href="/about">
                     About
                     </NavLink>
